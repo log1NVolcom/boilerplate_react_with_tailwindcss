50ce6e5da9ad2862f6defa0ddec5541f7d2fdc7b
log1NVolcom <joaorego_5@hotmail.com>
2020-10-06 01:23:12 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      maxHeight: {
+        64: '16rem',
+        72: '18rem',
+        84: '21rem',
+        96: '24rem',
+      },
+      spacing: {
+        72: '18rem',
+        84: '21rem',
+        96: '24rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
